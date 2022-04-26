bfe4054d2349d9d957ca6d121810fda2955e1b9b
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-04-26 12:17:58 +0300

    import pyperclip

--- credentials.py
+++ credentials.py
@@ -5,14 +5,14 @@ class Credentials:
     credentials_list = []
     # assign property to credential list
     def __init__(self,account,email,password):
-       self.account = account
-       self.email = email
-       self.password = password
-   def save_credentials(self):
-       '''
-       self credentials in credential_list
-       '''
-       Credentials.credentials_list.append(self)
+        self.account = account
+        self.email = email
+        self.password = password
+    def save_credentials(self):
+        '''
+        self credentials in credential_list
+        '''
+        Credentials.credentials_list.append(self)
 
     def delete_credentials(self):
         '''
